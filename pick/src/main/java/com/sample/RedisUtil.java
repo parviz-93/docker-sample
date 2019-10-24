d777718de7a84e759baec3f94b67578606cdfa1f
Parviz ROzikov <rozikovp@gmail.com>
2019-10-25 00:35:41 +0700

    sample deployment

--- pick/src/main/java/com/sample/RedisUtil.java
+++ pick/src/main/java/com/sample/RedisUtil.java
@@ -1,17 +1,29 @@
 package com.sample;
 
 import org.redisson.Redisson;
+import org.redisson.api.RList;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 
 public class RedisUtil {
 
-    private static String connect = "0.0.0.0:6379";
+    //private static String connect = "0.0.0.0:6379";
+    private static String connect = "redis-challenge8team7.apps.ocp.sbercloud.ru:6379";
 
     public static RedissonClient createClient() {
         Config config = new Config();
         config.useSingleServer()
-                .setAddress("redis://" + System.getProperty("redis", connect));
+                .setAddress("redis://" + connect);
         return Redisson.create(config);
+    }
+
+    public static void main(String[] args) {
+        final RedissonClient client = createClient();
+        final RList<Object> list = client.getList("list");
+        list.add("String1");
+        list.add("String2");
+
+        list.forEach(System.out::println);
+
     }
 }
