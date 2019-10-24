4a1edb85928b84be4897e29c09a57fb6800a17e8
kibirev <kibir@yandex.ru>
2019-10-25 01:16:36 +0700

    - add parametrs

--- transmitter/src/main/java/com/sample/transmitter/App.java
+++ transmitter/src/main/java/com/sample/transmitter/App.java
@@ -1,15 +1,35 @@
 package com.sample.transmitter;
 
-import java.util.List;
+import java.util.*;
 
 public class App {
-    public static void main(String[] args) {
-        Source source = new KafkaSource();
-        Sink sink = new KafkaSink(null,null);
+    public static void main(String[] args) throws Exception{
+        Source source = new KafkaSource(args[0],args[1],args[2]);
+        Sink sink = new KafkaSink(args[3],args[4]);
+
+        Set<Integer> set = new HashSet<>();
+        int doubicate = 0;
+        int hash;
 
         while(!Thread.interrupted()){
             List<byte[]> records = source.get();
-            sink.put(records);
+            System.out.println(records.size());
+
+            for (byte[] record : records) {
+                hash = Arrays.hashCode(record);
+                if(!set.contains(hash)){
+                    set.add(hash);
+                } else {
+                    doubicate++;
+                }
+            }
+
+            System.out.println("dublicates: " + doubicate);
+            System.out.println("origins   : " + set.size());
+
+//            sink.put(records);
         }
+
+        System.out.println("original: " + set.size());
     }
 }
