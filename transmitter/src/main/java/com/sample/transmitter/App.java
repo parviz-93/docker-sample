10ec4fa4d2e081fcfa8611be9365c822b19ef2c5
kibirev <kibir@yandex.ru>
2019-10-24 21:05:36 +0700

    - add KafkaSink

--- transmitter/src/main/java/com/sample/transmitter/App.java
+++ transmitter/src/main/java/com/sample/transmitter/App.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class App {
     public static void main(String[] args) {
         Source source = new KafkaSource();
-        Sink sink = new KafkaSink();
+        Sink sink = new KafkaSink(null,null);
 
         while(!Thread.interrupted()){
             List<byte[]> records = source.get();
